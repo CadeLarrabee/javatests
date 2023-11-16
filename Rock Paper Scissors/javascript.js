2ec3aa61c0247c473cc4c8e61952a50a1b5c6edb
Cade Larrabee <larrabeecade@gmail.com>
2023-11-16 12:49:38 -0500

    adding skeletons

---
+++ Rock Paper Scissors/javascript.js
@@ -0,0 +1,16 @@
+/*Write a function that plays a single round of Rock Paper Scissors. 
+The function should take two parameters - the playerSelection and 
+computerSelection - and then return a string that declares the winner 
+of the round like so: "You Lose! Paper beats Rock"
+Make your function’s playerSelection parameter case-insensitive 
+(so users can input rock, ROCK, RocK or any other variation).
+*/
+
+function getComputerChoice(){
+
+    return console.log(compChoice)
+}
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+}
+playRound("Rock",getComputerChoice());
