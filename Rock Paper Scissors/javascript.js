591309f9a29213cf4e86527ce916b2d14fecf1c8
Cade Larrabee <larrabeecade@gmail.com>
2023-11-24 13:37:35 -0500

    Added buttons, and text prompts

--- Rock Paper Scissors/javascript.js
+++ Rock Paper Scissors/javascript.js
@@ -6,6 +6,27 @@ Make your function’s playerSelection parameter case-insensitive
 (so users can input rock, ROCK, RocK or any other variation).
 */
 
+const btnrock = document.querySelector(".rock");
+const btnscissors = document.querySelector(".scissors");
+const btnpaper = document.querySelector(".paper");
+const playerActions = document.querySelector(".playerActions");
+
+const playerScore = document.querySelector(".playerScore");
+let playerScoreValue = 0;
+playerScore.textContent = playerScoreValue;
+
+btnrock.addEventListener('click', () => {
+    playRound('rock');
+});
+
+btnscissors.addEventListener('click', () => {
+    playRound('scissors');
+});
+
+btnpaper.addEventListener('click', () => {
+    playRound('paper');
+});
+
 function getComputerChoice(){
     let compChoice;
     let randNum = Math.floor(Math.random() * 3);
@@ -21,73 +42,59 @@ function getComputerChoice(){
     }
     return compChoice;
 }
+
 function playRound(playerSelection) {
     let computerSelection = getComputerChoice();
-    if(parseInt(playerSelection)){
-        console.log("Player has entered a number, please enter a string.")
-        return "error";
-    }
-    else{
-        playerSelection = playerSelection.toLowerCase();
-    }
 
     if(playerSelection === computerSelection) {
-        console.log("Draw!");
+        playerActions.textContent = 'Draw!';
         return 0;
     }
-    else if(playerSelection=="rock"){
+    if(playerSelection=="rock"){
         if(computerSelection=="scissors"){
-            console.log("Player Wins");
+            playerActions.textContent = 'Player Wins';
+            updatePlayerScore();
             return 1;
         }
         else if(computerSelection=="paper"){
-            console.log("Player Loses");
+            playerActions.textContent = 'Player Loses';
             return 0;
         }
     }
-    else if(playerSelection=="scissors"){
+    if(playerSelection=="scissors"){
         if(computerSelection=="paper"){
-            console.log("Player Wins");
+            playerActions.textContent = 'Player Wins';
+            updatePlayerScore();
             return 1;
         }
         else if(computerSelection=="rock"){
-            console.log("Player Loses");
+            playerActions.textContent = 'Player Loses';
             return 0;
         }
     }
-    else if(playerSelection=="paper"){
+    if(playerSelection=="paper"){
         if(computerSelection=="rock"){
-            console.log("Player Wins");
+            playerActions.textContent = 'Player Wins';
+            updatePlayerScore();
             return 1;
         }
         else if(computerSelection=="scissors"){
-            console.log("Player Loses");
+            playerActions.textContent = 'Player Loses';
             return 0;
         }
     }
-    else {
-        console.log("Player Has Entered an invalid input");
+    playerActions.textContent = 'Invalid Input!';
     return 0;
 }
+
+function updatePlayerScore(){
+    playerScoreValue += 1;
+    playerScore.textContent = playerScoreValue;
+    checkWinCondition();
 }
 
-function playFiveRounds(){
-    let roundswon = 0;
-    for(let i = 0;i < 5;i++){
-    roundswon = roundswon + playRound(prompt("Enter Rock Paper Scissors Choice"));
-    console.log("Player has won " + roundswon + " rounds");
-    }
-    return roundswon;
-}
-
-function playRockPaperScissors(){
-    let rounds = playFiveRounds();
-    if(rounds >= 3) {
-        return "Player wins the round of 5";
-    }
-    else {
-        return "Player Has Lost the round of 5";
+function checkWinCondition(){
+    if(playerScoreValue >= 5){
+        playerActions.textContent = 'Player Has Won the game!';
     }
 }
-
-playRockPaperScissors();
