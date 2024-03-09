b35661f6c0871a462e54aa7860f84150290007d0
Cade Larrabee <larrabeecade@gmail.com>
2024-03-09 11:26:43 -0500

    setting up the form basics

---
+++ FormValidationTest/main.js
@@ -0,0 +1,9 @@
+//email validation
+//must have a real email at the end, an @ symbol
+//country
+//probably a choice menu with every country in it
+//zip code
+//5 numbers
+//password
+//has to have a capital, an uppercase, and a !@%$# symbol
+//confirm password
