fbb87e30d99c0a72ec3f1fd27e42421c75022433
Cade Larrabee <larrabeecade@gmail.com>
2024-03-09 12:09:17 -0500

    final edit

--- FormValidationTest/main.js
+++ FormValidationTest/main.js
@@ -2,17 +2,20 @@ const passwordInput = document.getElementById("password");
 const confirmPasswordInput = document.getElementById("confirmPassword");
 const passwordError = document.getElementById("passwordError");
 
-// Function to validate password match
+//validate password matching
 function validatePasswordMatch() {
   if (passwordInput.value !== confirmPasswordInput.value) {
     passwordError.textContent = "Passwords do not match";
-    document.appendChild(passwordError);
   } else {
     passwordError.textContent = "";
-    document.removeChild(passwordError);
   }
 }
 
-// Add event listeners to trigger validation on blur
-passwordInput.addEventListener("blur", validatePasswordMatch);
+//check if passwords are correct when user clicks outside
 confirmPasswordInput.addEventListener("blur", validatePasswordMatch);
+const form = document.getElementById("myForm");
+
+form.addEventListener("submit", function (event) {
+  //don't submit because we're just testing
+  event.preventDefault();
+});
